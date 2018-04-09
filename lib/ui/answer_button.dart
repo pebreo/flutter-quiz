685523300ccf4b818d716ea6e38d4a2770ea756c
pjebz <pauljasper99@gmail.com>
2018-04-08 21:12:55 -0500

    add box border

--- lib/ui/answer_button.dart
+++ lib/ui/answer_button.dart
@@ -21,7 +21,12 @@ class AnswerButton extends StatelessWidget {
                       onTap: () => _onTap(), // enable anonymous function as a parameter
                       child: new Center(
                         child: new Container(
-                          child: new Text(this._answer == true ? "True": "False"),
+                          decoration: new BoxDecoration(
+                            border: new Border.all(color: Colors.white, width: 4.0)
+                          ),
+                          padding: new EdgeInsets.all(15.0),
+                          child: new Text(this._answer == true ? "True": "False", 
+                          style: new TextStyle(color: Colors.white, fontSize: 40.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
                         ),
                       ))));
   }
