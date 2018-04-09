e2da5d9f29bca97ba11bc936efcf8c5b48ecd009
pjebz <pauljasper99@gmail.com>
2018-04-08 20:36:24 -0500

    refactor for private variables

--- lib/ui/answer_button.dart
+++ lib/ui/answer_button.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
- 
-  final bool answer;
+  
+  // stateless widgets are immutable so they should have final type
+  final bool _answer;
  
  // add signature to a function to enable anonymous function as a param
   final VoidCallback _onTap;
  
  // enable anonymous function as paramater
-  AnswerButton(this.answer, this._onTap);
+  AnswerButton(this._answer, this._onTap);
 
   @override
   Widget build(BuildContext context) {
     return new Expanded(
               child: new Material(
                   // False button
-                  color: this.answer == true ? Colors.greenAccent : Colors.redAccent,
+                  color: this._answer == true ? Colors.greenAccent : Colors.redAccent,
                   child: new InkWell(
                       onTap: () => _onTap(), // enable anonymous function as a parameter
                       child: new Center(
                         child: new Container(
-                          child: new Text(this.answer == true ? "True": "False"),
+                          child: new Text(this._answer == true ? "True": "False"),
                         ),
                       ))));
   }
