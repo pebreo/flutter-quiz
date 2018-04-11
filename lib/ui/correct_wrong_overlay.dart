1e5cc9da4cd65e6490f95cce8a3bc1bd48a8bf48
pjebz <pauljasper99@gmail.com>
2018-04-10 19:02:27 -0500

    format overlay

--- lib/ui/correct_wrong_overlay.dart
+++ lib/ui/correct_wrong_overlay.dart
@@ -15,7 +15,13 @@ class CorrectWrongOverlayState extends State<CorrectWrongOverlay> {
           child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                new Container(child: new Icon(Icons.done, size: 80.0)),
+                new Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle
+                  ),
+                  child: new Icon(Icons.done, size: 80.0)
+                  ),
                 new Text("Correct!", style: new TextStyle(color: Colors.white, fontSize: 30.0))
               ],
           ),
